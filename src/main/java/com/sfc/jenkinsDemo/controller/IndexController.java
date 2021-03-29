5b5c00d48ac8c650f5ceea9f2aa42f2a2fc7f098
sfc1996 <941678171@qq.com>
2021-03-29 22:33:06 +0800

    测试内容更改！！！

--- src/main/java/com/sfc/jenkinsDemo/controller/IndexController.java
+++ src/main/java/com/sfc/jenkinsDemo/controller/IndexController.java
@@ -11,6 +11,6 @@ public class IndexController {
 
     @RequestMapping("index")
     public String index(){
-        return "spring boot demo from jenkins!";
+        return "spring boot demo from jenkins,content changed by github.";
     }
 }
